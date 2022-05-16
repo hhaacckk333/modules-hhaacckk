fed59bdfead2192810d56a9f7708c5913985417f
hhaacckk333 <49251963+hhaacckk333@users.noreply.github.com>
2022-05-16 12:27:00 +0300

    Update irisbot.py
    new code

--- irisbot.py
+++ irisbot.py
@@ -1,54 +1,103 @@
-from .. import loader		
-from asyncio import sleep
+# for more info: hhaacckk.tk
+# by hhaacckk, 2022
 
-class IrisBotMod(loader.Module):
-	"""Я рекомендую создать вам чат ,где вы будете использовать данный модуль, чтобы вас не беспокоили лишнии уведомления и тд. Так же можно это делать в лс Iris-а."""
-	strings = {"name": "IrisBot"}
-	
-	def __init__(self):
-		self.farm = True
-		self.virys = True
-		
-	async def farmcmd(self, message):
-		"""Включает команду "Ферма". Чтобы остановить, используйте "ирисфарм стоп"."""
-		while self.farm:
-			await message.reply("Ферма")
-			await sleep(14500)
-			
-	async def virysncmd(self, message):
-		"""Включает команду "Заразить =" (Заражает равного по силе соперника). Чтобы остановить, используйте "ирисвирус стоп"."""
-		while self.virys:
-			await message.reply("Заразить =")
-			await sleep(3600)
-			
-	async def virysecmd(self, message):
-		"""Включает команду "Заразить -" (Заражает слабого соперника). Чтобы остановить, используйте "ирисвирус стоп"."""
-		while self.virys:
-			await message.reply("Заразить -")
-			await sleep(3600)
-			
-	async def viryshcmd(self, message):
-		"""Включает команду "Заразить +" (Заражает сильного соперника) . Чтобы остановить, используйте "ирисвирус стоп"."""
-		while self.virys:
-			await message.reply("Заразить +\n\n<b>Следующая команда будет произведена через 1 час.\n\nIrisBot by @doyouwant13</b>")
-			await sleep(3600)
-			
-	async def watcher(self, message):
-		me = (await message.client.get_me())
-		if message.sender_id == me.id:
-			if message.text.lower() == "ирисфарм стоп":
-				self.farm = False
-				await message.reply("<b>Ирисфарм остановлен.</b>")
-			if message.text.lower() == "ирисвирус стоп":
-				self.virys = False
-				await message.reply("<b>Ирисвирус остановлен.</b>")
-				
-	async def linkcmd(self, message):
-		"""Если у вас не работают команды,используйте данную команду, чтобы заново установить модуль."""
-		await message.edit("<code>hhaacckk.tk</code>\n\n<b>IrisBot by @doyouwant13")
-		
-			
-			
+import random
+from .. import loader, utils
+from datetime import timedelta
+from telethon import functions
+from telethon.tl.types import Message
 
-		
-			
+
+@loader.tds
+class FarmIrisMod(loader.Module):
+    """Для автоматического фарминга коинов в ирисботе"""
+
+    strings = {
+        "name": "farmiris",
+        "farmon": "<i>✅Отложенка создана, автофарминг запущен, всё начнётся через 20 секунд...</i>",
+        "farmon_already": "<i>Уже запущено</i>",
+        "farmoff": "<i>❌Автофарминг остановлен.\n☢️Надюпано:</i> <b>%coins% i¢</b>",
+        "farm": "<i>☢️Надюпано:</i> <b>%coins% i¢</b>",
+    }
+
+    def __init__(self):
+        self.name = self.strings["name"]
+
+    async def client_ready(self, client, db):
+        self.client = client
+        self.db = db
+        self.myid = (await client.get_me()).id
+        self.iris = 707693258
+
+    async def farmoncmd(self, message):
+        """Запустить автофарминг"""
+        status = self.db.get(self.name, "status", False)
+        if status:
+            return await message.edit(self.strings["farmon_already"])
+        self.db.set(self.name, "status", True)
+        await self.client.send_message(
+            self.iris, "Фарма", schedule=timedelta(seconds=20)
+        )
+        await message.edit(self.strings["farmon"])
+
+    async def farmoffcmd(self, message):
+        """Остановить автофарминг"""
+        self.db.set(self.name, "status", False)
+        coins = self.db.get(self.name, "coins", 0)
+        if coins:
+            self.db.set(self.name, "coins", 0)
+        await message.edit(self.strings["farmoff"].replace("%coins%", str(coins)))
+
+    async def farmcmd(self, message):
+        """Вывод кол-ва коинов, добытых этим модулем"""
+        coins = self.db.get(self.name, "coins", 0)
+        await message.edit(self.strings["farm"].replace("%coins%", str(coins)))
+
+    async def watcher(self, event):
+        if not isinstance(event, Message):
+            return
+        chat = utils.get_chat_id(event)
+        if chat != self.iris:
+            return
+        status = self.db.get(self.name, "status", False)
+        if not status:
+            return
+        if event.raw_text == "Фарма":
+            return await self.client.send_message(
+                self.iris, "Фарма", schedule=timedelta(minutes=random.randint(1, 20))
+            )
+        if event.sender_id != self.iris:
+            return
+        if "НЕЗАЧЁТ!" in event.raw_text:
+            args = [int(x) for x in event.raw_text.split() if x.isnumeric()]
+            randelta = random.randint(20, 60)
+            if len(args) == 4:
+                delta = timedelta(
+                    hours=args[1], minutes=args[2], seconds=args[3] + randelta
+                )
+            elif len(args) == 3:
+                delta = timedelta(minutes=args[1], seconds=args[2] + randelta)
+            elif len(args) == 2:
+                delta = timedelta(seconds=args[1] + randelta)
+            else:
+                return
+            sch = (
+                await self.client(
+                    functions.messages.GetScheduledHistoryRequest(self.iris, 1488)
+                )
+            ).messages
+            await self.client(
+                functions.messages.DeleteScheduledMessagesRequest(
+                    self.iris, id=[x.id for x in sch]
+                )
+            )
+            return await self.client.send_message(self.iris, "Фарма", schedule=delta)
+        if "ЗАЧЁТ" in event.raw_text or "УДАЧА" in event.raw_text:
+            args = event.raw_text.split()
+            for x in args:
+                if x[0] == "+":
+                    return self.db.set(
+                        self.name,
+                        "coins",
+                        self.db.get(self.name, "coins", 0) + int(x[1:]),
+                    )
