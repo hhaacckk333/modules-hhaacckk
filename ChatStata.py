6b4f73c862fa1ea9a01aa546135b9f0df060da7a
hhaacckk.tk <49251963+hhaacckk333@users.noreply.github.com>
2022-07-20 19:06:36 +0300

    Add files via upload

---
+++ ChatStata.py
@@ -0,0 +1,112 @@
+#
+#                     Copyright 2022 t.me/hhaacckk1
+#           Licensed under the Creative Commons CC BY-NC-ND 4.0
+#
+#                    Full license text can be found at:
+#       https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+#
+#                           Human-friendly one:
+#            https://creativecommons.org/licenses/by-nc-nd/4.0
+
+# meta developer: @hhaacckk1 
+
+
+from telethon.tl.types import *
+
+from .. import loader
+
+
+@loader.tds
+class ChatStatisticMod(loader.Module):
+    "Статистика чата"
+    strings = {"name": "ChatStatistic"}
+
+    @loader.owner
+    async def statacmd(self, m):
+        await m.edit("<b>Считаем...</b>")
+        al = str((await m.client.get_messages(m.to_id, limit=0)).total)
+        ph = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterPhotos()
+                )
+            ).total
+        )
+        vi = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterVideo()
+                )
+            ).total
+        )
+        mu = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterMusic()
+                )
+            ).total
+        )
+        vo = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterVoice()
+                )
+            ).total
+        )
+        vv = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterRoundVideo()
+                )
+            ).total
+        )
+        do = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterDocument()
+                )
+            ).total
+        )
+        urls = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterUrl()
+                )
+            ).total
+        )
+        gifs = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterGif()
+                )
+            ).total
+        )
+        geos = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterGeo()
+                )
+            ).total
+        )
+        cont = str(
+            (
+                await m.client.get_messages(
+                    m.to_id, limit=0, filter=InputMessagesFilterContacts()
+                )
+            ).total
+        )
+        await m.edit(
+            (
+                "<b>Всего сoообщений</b> {}\n"
+                + "<b>Фоток:</b> {}\n"
+                + "<b>Видосов:</b> {}\n"
+                + "<b>Попсы:</b> {}\n"
+                + "<b>Голосовых:</b> {}\n"
+                + "<b>Кругляшков:</b> {}\n"
+                + "<b>Файлов:</b> {}\n"
+                + "<b>Ссылок:</b> {}\n"
+                + "<b>Гифок:</b> {}\n"
+                + "<b>Координат:</b> {}\n"
+                + "<b>Контактов:</b> {}"
+            ).format(al, ph, vi, mu, vo, vv, do, urls, gifs, geos, cont)
+        )
