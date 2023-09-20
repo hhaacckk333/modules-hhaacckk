86e2bfaef9cde39709e222143ac88b07bc6e0b99
hhaacckk.tk <49251963+hhaacckk333@users.noreply.github.com>
2023-09-20 20:58:03 +0300

    Update GBsliv.py

--- GBsliv.py
+++ GBsliv.py
@@ -21,25 +21,25 @@ class EyeCheckerTGMod(loader.Module):
         self._db = db
         self._client = client
 
-    async def checkcmd(self, m):
+    async def gbuid(self, m):
         """ Проверить uid на номер
 Отправляет данные в чат
 Жуёт либо <reply>, либо <uid>"""
         await check(m, self.strings['check'], self.strings['version'])
 
-    async def pcheckcmd(self, m):
+    async def gbsliv(self, m):
         """ Проверить номер на наличие в бд
 Отправляет данные в чат
 Жуёт либо <reply>, либо <phone>"""
         await check(m, self.strings['check'], self.strings['version'], 'p')
 
-    async def scheckcmd(self, m):
+    async def gbcheck(self, m):
         """ Аналогично check
 Отправляет данные в избранное
 Жуёт либо <reply>, либо <uid>"""
         await check(m, self.strings['check'], self.strings['version'], save=True)
 
-    async def spcheckcmd(self, m):
+    async def gbslivsilent(self, m):
         """ Аналогично pcheck
 Отправляет данные в избранное
 Жуёт либо <reply>, либо <phone>"""
